460f8a93dce20de525c5f4eabdb47b3bc5becd36
yeargin2021 <88904457+yeargin2021@users.noreply.github.com>
2022-06-01 05:44:13 -0400

    Commit

---
+++ 30Days/01_Day_01.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i2 = 12
+# Read and save an integer, double, and String to your variables.
+d2 = 4.0
+s2 = "is the best place to learn and practice coding!"
+# Print the sum of both integer variables on a new line.
+i = i + i2
+d = d + d2
+s = s + s2
+# Print the sum of the double variables on a new line.
+#print(s)
+print(i)
+print(d)
+print(s)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
---
+++ challenges/202206010527.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Basic range with steps for loop
+for value in {1..99..2}
+do
+echo $value
+done
